082d110b05357debdaac4e0cbcf819989f5a0a0c
Pratyush Bhaskar <pratyush.bhaskar08@gmail.com>
2025-06-06 23:28:50 +0530

    Add user channel profile and watch history retrieval features

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -307,6 +307,8 @@ const updateUserAvatar = asyncHandler( async (req, res) => {
         }
     ).select("-password")
 
+    // TODO: Delete old avatar from cloudinary
+
     return res
     .status(200)
     .json(new ApiResponse(200, user, "Avatar updated successfully"))
@@ -340,6 +342,122 @@ const updateUserCoverImage = asyncHandler( async (req, res) => {
     .json(new ApiResponse(200, user, "Cover image updated successfully"))
 })
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const {username} = req.params;
+
+    // if(!username || username.trim() === "") {
+    if(!username.trim()) {
+        throw new ApiError(400, "Username is missing");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions", // In model Subscription will become subscriptions
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: { $size: "$subscribers" },
+                channelsSubscribedToCount: { $size: "$subscribedTo" },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+            }
+        }
+    ])
+
+    // if(!channel || channel.length === 0) {
+    if(!channel?.length) {
+        throw new ApiError(404, "Channel does not exist");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "Channel profile fetched successfully"))
+})
+
+const getUserHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id) // Understand the use of new mongoose.Types.ObjectId() to convert string to ObjectId and why we cannot directly use req.user._id
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",  // In model Video will become videos
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users", // In model User will become users
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: { $arrayElemAt: ["$owner", 0] } // Get the first element of the owner array
+                            // This is because the lookup returns an array of objects, we need to get the first object
+                            // We can also do $first: "$owner" instead of $arrayElemAt
+                        }
+                    }
+                ]
+            }
+        },
+
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user[0].watchHistory, "User watch history fetched successfully"))
+})
+
 export { 
     registerUser, 
     loginUser, 
@@ -349,5 +467,7 @@ export {
     getCurrentUser, 
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getUserHistory
 };
